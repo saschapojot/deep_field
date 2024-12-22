4560d03d5f1b1a3fc45a5e7e8e18c5fc6dd56a17
Xi LIU <xi.liu@u.nus.edu>
2024-12-22 19:37:37 +0800

    train

--- double_quantum/lin_train.py
+++ double_quantum/lin_train.py
@@ -2,15 +2,13 @@ from config_lin import *
 import pickle
 import sys
 from datetime import datetime
-# from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression
 import joblib
 from pathlib import Path
-from sklearn.linear_model import SGDRegressor
 
 inDir=f"./train_test_data/N{N}/"
 
-num_samples_in=200000
-in_pkl_train_file=inDir+f"/db.train_num_samples{num_samples_in}.pkl"
+in_pkl_train_file=inDir+"/db.train.pkl"
 
 with open(in_pkl_train_file,"rb") as fptr:
     X_train, Y_train=pickle.load(fptr)
@@ -19,41 +17,35 @@ with open(in_pkl_train_file,"rb") as fptr:
 X_train_array = np.array(X_train)  # Shape: (num_samples, 3,N, N, )
 Y_train_array = np.array(Y_train)  # Shape: (num_samples,)
 
-tFeaturesStart=datetime.now()
-# Extract features for all samples in X_train_array
-num_samples = X_train_array.shape[0]
-# truncate=100
-features_list = []
-
-for i in range(num_samples):
-    Sigma_combined = X_train_array[i]  # Shape: (3, N, N)
-    features = generate_interaction_features_combined(
-        Sigma_combined, unique_pairs_with_displacements, four_body_combinations_with_displacements
-    )
-    features_list.append(features)
-    if i%1000==0:
-        print(f"processed i={i}")
-
-
-# Convert features list to a NumPy array
-X_train_features = np.array(features_list)  # Shape: (num_samples, num_features)
-
-tFeaturesEnd=datetime.now()
-
-print(f"feature time: {tFeaturesEnd-tFeaturesStart}")
-
-regressor = SGDRegressor()
-batch_size = 1000
-num_samples = X_train_features.shape[0]
+# tFeaturesStart=datetime.now()
+# # Extract features for all samples in X_train_array
+# num_samples = X_train_array.shape[0]
+# # truncate=100
+# features_list = []
+#
+# for i in range(num_samples):
+#     Sigma_combined = X_train_array[i]  # Shape: (3, N, N)
+#     features = generate_interaction_features_combined(
+#         Sigma_combined, unique_pairs_with_displacements, four_body_combinations_with_displacements
+#     )
+#     features_list.append(features)
+#     if i%1000==0:
+#         print(f"processed i={i}")
+#
+#
+# # Convert features list to a NumPy array
+# X_train_features = np.array(features_list)  # Shape: (num_samples, num_features)
+lin_model_out_dir="./lin_model_out/"
+features_in_file = lin_model_out_dir + "/X_train_features.pkl"
+with open(features_in_file, "rb") as fptr:
+    X_train_features = pickle.load(fptr)
 tFitStart=datetime.now()
 # Initialize the regression model with fit_intercept=True (default behavior)
-# regressor = LinearRegression(fit_intercept=True)
+regressor = LinearRegression(fit_intercept=True)
 
 # Fit the model
-# regressor.fit(X_train_features, Y_train_array)
-for start in range(0, num_samples, batch_size):
-    end = start + batch_size
-    regressor.partial_fit(X_train_features[start:end], Y_train_array[start:end])
+regressor.fit(X_train_features, Y_train_array)
+
 lin_model_out_dir="./lin_model_out/"
 Path(lin_model_out_dir).mkdir(exist_ok=True,parents=True)
 
