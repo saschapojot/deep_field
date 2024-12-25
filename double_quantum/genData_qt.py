920ecac216eaf0d1b0eb171888e8b5a115122ee2
LIU Xi <xi.liu@u.nus.edu>
2024-12-25 21:09:13 +0800

    ratio

--- double_quantum/genData_qt.py
+++ double_quantum/genData_qt.py
@@ -212,7 +212,7 @@ tStart=datetime.now()
 
 
 I_N2=np.eye(N**2)
-num_samples=200000
+num_samples=40000
 seed_vec=range(100,100+num_samples)
 data,values=gen_dataset(num_samples,N,T0_mat,T1_mat,T2_mat,T3_mat,t,J,mu,I_N2,T,seed_vec)
 
@@ -236,7 +236,7 @@ def generate_train_test(data,values,train_ratio):
 
 
 
-train_ratio=0.8
+train_ratio=0
 
 
 
