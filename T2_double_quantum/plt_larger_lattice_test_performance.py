0c6a64fb6b10fd7b1f5749b96b7f8c24641cd393
Xi Liu <xi.liu@u.nus.edu>
2025-01-05 18:16:50 +0800

    test

--- T2_double_quantum/plt_larger_lattice_test_performance.py
+++ T2_double_quantum/plt_larger_lattice_test_performance.py
@@ -8,21 +8,28 @@ from model_qt_dsnn_config import *
 
 #this script plots the test performance of model trained on 10 by 10 lattice on larger lattices
 
-layer_num=2
-C=30
+layer_num=0
+C=25
 
-N_vec=[10,15,20,25,30]
+N_vec=[10,15]
+step_num_after_S1=0
 
+decrease_over = 50
+
+decrease_rate = 0.95
+
+num_data_train_test=200000
+num_epochs = 1200
 inDirRoot="./larger_lattice_test_performance/"
-
+num_suffix=40000
 def N_2_test_file(N):
     in_model_dir = f"./larger_lattice_test_performance/N{N}/C{C}/layer{layer_num}/"
-    test_txt_file=in_model_dir+f"/test_over50_rate0.6_epoch1000_num_samples40000.txt"
+    test_txt_file=in_model_dir+f"/test_over{decrease_over}_rate{decrease_rate}_epoch{num_epochs}_num_samples{num_suffix}.txt"
     return test_txt_file
 
 def N_2_test_data_pkl(N):
     in_model_dir = f"./larger_lattice_test_performance/N{N}/C{C}/layer{layer_num}/"
-    pkl_test_file=in_model_dir+"/db.test_num_samples40000.pkl"
+    pkl_test_file=in_model_dir+f"/db.test_num_samples{num_suffix}.pkl"
     return pkl_test_file
 
 pattern_std=r'std_loss=\s*([+]?(?:\d*\.\d+|\d+)(?:[eE][-+]?\d+)?)'
@@ -58,25 +65,13 @@ for j in range(0,len(pkl_file_vec)):
 std_loss_vec=np.array(std_loss_vec)
 abs_avg_Y_train=np.array(abs_avg_Y_train_vec)
 N_2_vec=np.array(N_vec)**2
-relative_error=std_loss_vec/N_2_vec
+relative_error=std_loss_vec/abs_avg_Y_train
 N_vec=np.array(N_vec)
 # N_vec=N_vec.reshape(-1, 1)
 # log_N = np.log(N_vec).reshape(-1, 1)
 log_relative_error = np.log(relative_error)
-# Define the model
-def model(N, a, b, c,d):
-    return b * (N+d)**a + c
-params, covariance = curve_fit(model, N_vec, relative_error, p0=(-0.5, 10.0, 0.0,6),
-                               bounds=([-np.inf, 0, -np.inf,0], [0, np.inf, np.inf, np.inf])  # Example bounds
-                               )
-# Extract parameters
-a, b, c ,d= params
-print("Fitted parameters:")
-print(f"a = {a}, b = {b}, c = {c},d={d}")
 
-new_N_vec=np.array([10,15,20,25,30,40,50,60,70,80,90,100])
-predicted_relative_error_new = model(new_N_vec, a, b, c,d)
-print("Predicted relative_error for new N_vec:", predicted_relative_error_new)
+
 
 plt.figure()
 plt.scatter(N_vec,relative_error,color="blue",marker="o")
@@ -91,12 +86,3 @@ plt.savefig(inDirRoot+"/larger_lattice_test_performance.png")
 plt.close()
 
 
-plt.scatter(N_vec, relative_error, label="Original Data")
-plt.plot(N_vec, model(N_vec, a, b, c,d), label="Original Fit", color="red")
-plt.scatter(new_N_vec, predicted_relative_error_new, label="New Predictions", color="green")
-plt.xlabel("N")
-plt.ylabel("relative_error")
-plt.legend()
-plt.title("Model Fit and Predictions")
-plt.savefig(inDirRoot+"/fitted.png")
-plt.close()
